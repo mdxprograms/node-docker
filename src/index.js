37fcf0264eb3413f1cbcc0be0d1f6c974d0b38a2
Josh Waller <mdx.programs@gmail.com>
2020-03-27 17:50:00 -0400

    init starter files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Node Docker!!!');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);

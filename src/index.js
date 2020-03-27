fa88a4b05475a860e8f1f1dbcda4ae77995cd2e4
Josh Waller <mdx.programs@gmail.com>
2020-03-27 19:12:11 -0400

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
-// Constants
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Node Docker!!!');

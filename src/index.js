7f2613d3006a7a1ebb04d7795b74d1c871936f67
Josh Waller <mdx.programs@gmail.com>
2020-03-27 23:18:18 -0400

    formatted

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Node Docker!!!');
-});
+app.get("/", (req, res) =>
+  res
+    .json({
+      message: "Hello"
+    })
+    .status(200)
+);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
